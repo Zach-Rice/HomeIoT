0bbdd6e71563b65cdeb69c8daf47bc128541bf57
Zach-Rice <48661019+Zach-Rice@users.noreply.github.com>
2021-10-11 09:56:16 -0400

    Arduino Library update

--- obj/Debug/net5.0/RazorDeclaration/Pages/Temp.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Temp.razor.g.cs
@@ -13,91 +13,91 @@ namespace HomeIoT.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\zrice\Desktop\IOT project\HomeIoT\_Imports.razor"
+#line 1 "C:\Users\zrice\Desktop\IOTproject\HomeIoT\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\zrice\Desktop\IOT project\HomeIoT\_Imports.razor"
+#line 2 "C:\Users\zrice\Desktop\IOTproject\HomeIoT\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\zrice\Desktop\IOT project\HomeIoT\_Imports.razor"
+#line 3 "C:\Users\zrice\Desktop\IOTproject\HomeIoT\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\zrice\Desktop\IOT project\HomeIoT\_Imports.razor"
+#line 4 "C:\Users\zrice\Desktop\IOTproject\HomeIoT\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\zrice\Desktop\IOT project\HomeIoT\_Imports.razor"
+#line 5 "C:\Users\zrice\Desktop\IOTproject\HomeIoT\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\zrice\Desktop\IOT project\HomeIoT\_Imports.razor"
+#line 6 "C:\Users\zrice\Desktop\IOTproject\HomeIoT\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\zrice\Desktop\IOT project\HomeIoT\_Imports.razor"
+#line 7 "C:\Users\zrice\Desktop\IOTproject\HomeIoT\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\zrice\Desktop\IOT project\HomeIoT\_Imports.razor"
+#line 8 "C:\Users\zrice\Desktop\IOTproject\HomeIoT\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\zrice\Desktop\IOT project\HomeIoT\_Imports.razor"
+#line 9 "C:\Users\zrice\Desktop\IOTproject\HomeIoT\_Imports.razor"
 using HomeIoT;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\zrice\Desktop\IOT project\HomeIoT\_Imports.razor"
+#line 10 "C:\Users\zrice\Desktop\IOTproject\HomeIoT\_Imports.razor"
 using HomeIoT.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\zrice\Desktop\IOT project\HomeIoT\_Imports.razor"
+#line 11 "C:\Users\zrice\Desktop\IOTproject\HomeIoT\_Imports.razor"
 using HomeIoT.Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\zrice\Desktop\IOT project\HomeIoT\_Imports.razor"
+#line 12 "C:\Users\zrice\Desktop\IOTproject\HomeIoT\_Imports.razor"
 using Blazor.Extensions;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\zrice\Desktop\IOT project\HomeIoT\Pages\Temp.razor"
+#line 3 "C:\Users\zrice\Desktop\IOTproject\HomeIoT\Pages\Temp.razor"
 using System.Threading;
 
 #line default
@@ -112,7 +112,7 @@ using System.Threading;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 54 "C:\Users\zrice\Desktop\IOT project\HomeIoT\Pages\Temp.razor"
+#line 54 "C:\Users\zrice\Desktop\IOTproject\HomeIoT\Pages\Temp.razor"
        
     private string temperature { get; set; } = "0";
    Timer _update;
